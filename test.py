1570bb2274efb7c16782a29a56fc5b4c3e2c8c06
Rahul Kumar <rahulbit10180@gmail.com>
2019-01-13 20:38:39 +0530

    Added another function

--- test.py
+++ test.py
@@ -2,6 +2,10 @@
 
 def test_function():
 	print ("Now this is from feature-1 branch")
+
+def another_function():
+	print ("This is another function")
 	
 if __name__ == '__main__':
 	test_function()
+	another_function()
